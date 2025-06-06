c3d81bd40931196bd12f6469f817065c105feb97
i27devopsb6 <i27devops+b6git@gmail.com>
2025-06-06 21:52:08 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 const produtsServiceLocalHost = 'http://localhost:8132/api'
 const usersServiceLocalHost = 'http://localhost:8232/api'
 
-const produtsServiceDevHost = 'http://104.198.52.21:30263/api' 
-const usersServiceDevHost = 'http://104.198.52.21:31904/api'
+const produtsServiceDevHost = 'http://34.9.167.22:30872/api' 
+const usersServiceDevHost = 'http://34.9.167.22:31429/api'
 
 const produtsServiceTestHost = ''
 const usersServiceTestHost = ''
